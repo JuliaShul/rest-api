ed4a81065169b127c92a8632dbca9c8fbc194ced
shulbaeva <j.shulbaeva@cft.ru>
2021-06-07 20:22:46 +0700

    change on Condition.visible

--- src/test/java/qaguru/allure/WebSteps.java
+++ src/test/java/qaguru/allure/WebSteps.java
@@ -1,11 +1,11 @@
 package qaguru.allure;
 
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -34,7 +34,7 @@ public class WebSteps {
 
     @Step("Проверяем что Issue c номеро {number} существует")
     public void shouldSeeIssueWithNumber(int number) {
-        $(withText("#" + number)).should(Condition.visible);
+        $(withText("#" + number)).should(visible);
     }
 
     @Attachment(value = "Screenshot", type = "image/png")
